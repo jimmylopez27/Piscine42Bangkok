4bb8d4ab44240b8f86b3e3f1abdc764c4d222503
Jimmy <jbalayanan@gmail.com>
2026-01-07 23:54:59 +0700

    I halfy solve the reverse of a number problem

--- c-practice/pract6.c
+++ c-practice/pract6.c
@@ -6,29 +6,18 @@ int main()
   printf("Type your number here: ");
   int num;
   scanf("%d", &num);
-  for (int i = 1; i < 4; i ++ )
+  printf("The reverse of the number %d is : ", num);
+  for (int i = 1; i < 10; i ++ )
   {
    int div = pow(10, i);
-   printf("%d\n", div);
-   int rem = num % div;
-   printf("%.1d\n", rem);
+   int rem = (num * 10 / div) % 10;
+   printf("%d", rem);
    
-   num = rem;   
+   if (rem < 1 )
+   {
+    break;
+   }
   };
- 
- }
 
- 
-/*  printf("\nType the number here: ");
- int num;
- scanf("%d", &num);
-
- float remaindernum =  num % 10; 
- float wholenum = (num / 10); 
- 
- printf("Whole number: %.0f \nRemainder: %.0f \n", wholenum, remaindernum);
-
- printf("The reverse of the number %d is: %.0f%.0f\n\n", num, remaindernum, wholenum);
- 
- return 0; */  
-
+ return 0;
+}
