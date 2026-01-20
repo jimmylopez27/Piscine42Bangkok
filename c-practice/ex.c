1166e8751237a5d39f07110108a481be5fb91c92
Jimmy <jbalayanan@gmail.com>
2026-01-21 00:13:24 +0700

    I still try to solve the how many unique numbers and what are those in array

--- c-practice/ex.c
+++ c-practice/ex.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-	int n;
-	printf("Type your n: " );
-	scanf("%d", &n);
+	char num[] = {2, 4, 6};
 	
-	int *p = malloc(n * sizeof(int));
-	printf("The *p is %d\n", *p);
-	printf("The address of where the memory is stored is %p\n", p); 
-
-	p[0] = 7;
-	p[1] = 9;
-	p[2] = 10;
-	printf("The *p is %d\n", *p);
-	printf("The p[1] is %d\n", p[1]);
-	printf("The p[2] is %d\n", p[2]);
-
-	printf("The address of p[0] is %p\n", &p[0]);
-	printf("The address of p[1] is %p\n", &p[1]);
-	printf("The address of p[2] is %p\n", &p[2]);
+	int count = 0;	
+	while (num[count] != NULL )
+	{
+		if (num != NULL)
+		{
+			printf("Num: %d\n", num[count]);
+			count++;
+		}
 	
-
+	}
 	return 0;
 }
