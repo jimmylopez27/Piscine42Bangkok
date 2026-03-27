08a7d4d5ff0104ccbe7afaf340a6f0c2a1dff66e
Jimmy <jbalayanan@gmail.com>
2026-03-27 23:26:13 +0700

    new

--- activities/openfile.c
+++ activities/openfile.c
@@ -2,11 +2,11 @@
 
 #define BUFF 1000
 
-int char_count(char *chars)
+int char_count(char *str)
 {
 	int i = 0;
 	
-	while(chars[i] != '\0')
+	while(str[i] != '\0')
 	{
 		i++;
 	}
@@ -14,33 +14,61 @@ int char_count(char *chars)
 	return i;
 }
 
-char int_to_char(int num)
+void int_to_char(int num_length, char *str_length)
 {
+	int n = num_length;
+	int m = 0;
 	int i = 0;
-	char *str;
 
-	while(num[i] >= '0' && num[i] <= '9')
+	if(num_length == 0)
 	{
-		str = str * 10 + (num[i] - 0);
-		i++;
+		str_length[i++] = '0';
+		str_length[i] = '\0';
+		return;
 	}
 
-	return *str;
+	while(n > 0)
+	{
+		n = n / 10;
+		m++;
+	}	
+	
+	str_length[m] = '\0';
+	
+	while(m > 0)
+	{
+		str_length[--m] = (num_length % 10) + '0';
+		num_length = num_length / 10;
+	}
 }
 
-int main ()
+int main()
 {
 	char word[BUFF];
+	int word_length;
+	char digits_length[BUFF];
+
+	write(1, "Type your word here: ", 21);
+	read(0, word, BUFF - 1);	
+	int n = char_count(word);
 	
-	write(1, "Write your words here: ",23 );
-	read(1, word, BUFF);
+
+	word_length = char_count(word);
 	
-	int len = char_count(word);
 	
-	write(1, word, len);
+	write(1, "\n", 2);
+	write(1, "Your word is: ", 14);
+	write(1, word, word_length);	
 	
-	char lenchar = int_to_char(len);
-	write(1, lenchar, len);
+	int_to_char(word_length, digits_length);	
+
+	write(1, "The length of the word is: ", 27); 
+	write(1, digits_length, char_count(digits_length));
+	write(1, "\n", 2);
+	write(1, "\n", 2);
+	
+
 
 	return 0;
 }
+
