59ec6fd2d7c56fb48b691f659b9f042ac3d9a326
Jimmy <jbalayanan@gmail.com>
2026-04-04 10:27:03 +0700

    Hello

--- c-lessons/42Logo/42logo.c
+++ c-lessons/42Logo/42logo.c
@@ -1,73 +1,97 @@
 #include <unistd.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-// #define HEIGHT 37
-// #define WIDTH 131
+#define HEIGHT 20
+#define WIDTH 100
 
-int char_to_int(char *str)
-{
-	char temp = *str;
-	int n = 0;
+#define BUFF 200
 
-	while(str[n] != '\0')
-	{
-		n++;
-	}
-
-	int num;
-	while(n > 0)
-	{
-		num[--n] = (temp % 10) + '0';
-	}
-
-	return num;
-}
-
-void plot(int x, int y)
-{
-	int xcounter = 0;
-	int ycounter = 0;
-
-	while(ycounter <= y)
-	{
-	
-		if(ycounter == y)
-		{
-			while(xcounter <= x)
-			{
-				if(xcounter == x)
-				{
-					write(1, "#", 1);
-					write(1, "\n", 1);
-				}
-				else
-				{
-					write(1, " ", 1);
-				}
-				xcounter++;
-			}
-		}
-		else
-		{
-			write(1, "\n", 1);
-		}
-
-		ycounter++;
-	}
-
-}
+void plot(int x, int y);
+int char_int(int str_num);
 
 int main()
 {
-	int x = 0, y = 0;
-	write(1, "x coordinate: \n", 17);
-	read(1, x, 1);
-	write(1, "y coordinate: \n", 17);
-	read(1, y, 1);
+	char x;
+	char y;
+	char buff;
+	write(1, "x coordinate: ", 14);
+	read(0, &x, 1);
+	read(0, &buff, 1);	
 
-	char_to_int(x, y);		
+	write(1, "y coordinate: ", 14);
+	read(0, &y, 1);
+	read(0, &buff, 1);
+
+	int x_int = char_int(x);
+	int y_int = char_int(y);
 	
-	plot(x, y);	
+
+//	printf("X = %d and Y = %d\n", x_int, y_int);
+//	plot(x_int, y_int);	
+
 
 	return 0;
+}
+
+
+
+int char_int(int str_num)
+{
+	char temp = str_num;
+	int count = 0;
+
+	while(temp != NULL)
+	{
+		count++;
+		temp++;	
+	}	 
+
+	int int_num;
+	
+	while(count > 0)
+	{
+		int_num = (str_num % 10) + '0';
+		count--;
+		int_num++;
+		str_num = str_num / 10;
+	}
+	
+	return int_num;
+}
+
+
+void plot(int x, int y)
+{
+        int xcounter = 0;
+        int ycounter = 0;
+
+        while(ycounter <= y)
+        {
+
+                if(ycounter == y)
+                {
+                        while(xcounter <= x)
+                        {
+                                if(xcounter == x)
+                                {
+                                        write(1, "#", 1);
+                                        write(1, "\n", 1);
+                                }
+                                else
+                                {
+                                        write(1, " ", 1);
+                                }
+                                xcounter++;
+                        }
+                }
+                else
+                {
+                        write(1, "\n", 1);
+                }
+
+                ycounter++;
+        }
+
 }
--- c-lessons/42Logo/chat_int.c
+++ c-lessons/42Logo/chat_int.c
@@ -1,57 +1,118 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define BUFF 200
 
-int word_len(char *str)
-{
-	char temp = *str;
-	int len = 0;
-	while(temp != '\0')
-	{	
-		temp++;
-		len++;
-	}
-	return len;
-}
-
-int digits(int len)
-{
-	int temp = len;
-	int count;
-	while(temp > 0)
-	{
-		temp /= 10;
-		temp;
-		count++;
-	}
-	return count;
-}
-
-char digit_char(int digits)
-{
-	char *namChar;
-	int n = digits;
-	
-	while(n > 0)
-	{
-		namChar[--n] = (digits % 10) + '0';
-		digits \= 10;
-	}	
-	
-	namChar[]
-	
-	retun *namChar;
-}
+int word_len(char *str);
+int digits(int len);
+char *digits_char(int digits, int len);
+void newline_remover(char *word, int bytes);
 
 int main()
 {
 	char word[BUFF];
 	write(1, "Type your word: ", 16);
-	read(1, word, BUFF);
-	
-	int n = word_len(word);
-	char str = int_char(n);	
-	write(1, *str, n);	
-	
+	ssize_t bytes;
+	bytes = read(0, word, BUFF);	
+	newline_remover(word, bytes);
+
+	int len = word_len(word);
+	int dig = digits(len);
+	char *str = digits_char(len, dig);
+
+	write(1, "The length of the word is: ", 27);	
+	write(1, str, dig);
+	write(1, "\n", 2);
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int word_len(char *str)
+{
+	char *temp = str;
+	int len = 0;
+	while(*temp != '\0')
+	{	
+		temp++;
+		len++;
+	}
+	printf("The length is %d\n", len);
+	return len;
+}
+
+int digits(int len)
+{
+	int count = 0;
+	if(len == 0)
+	{
+		return 1;
+	}
+
+	while(len > 0)
+	{
+		len = len / 10;
+		count++;
+	}
+	printf("The count is:  %d\n", count);
+	return count;
+}
+
+char *digits_char(int digits, int len)
+{
+	int n = len;
+	char *namChar = malloc(len + 1);
+	
+	if(!namChar)
+	{
+		return NULL;
+	}
+	
+	while(n > 0)
+	{
+		namChar[--n] = (digits % 10) + '0';
+		digits = digits / 10;
+	}
+	
+	namChar[len] = '\0';
+	return namChar;
+}
+
+	
+void newline_remover(char *word, int bytes)
+{
+    int i = 0;
+
+    while (i < bytes)
+    {
+        if (word[i] == '\n')
+        {
+            word[i] = '\0';
+            return;
+        }
+        i++;
+    }
+
+    // fallback (no '\n' found)
+    word[bytes] = '\0';
+}
+
+
+
+
